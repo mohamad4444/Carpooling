65466bfdb6b9e5d457b0e32c1557360df6d1c14a
Mohammad Mahamid <61114936+mohamad4444@users.noreply.github.com>
2025-06-19 10:51:38 +0200

    moved changetap to style.js

--- carpooling-app/src/main/resources/static/js/style.js
+++ carpooling-app/src/main/resources/static/js/style.js
@@ -17,3 +17,23 @@ function showloginDiv()
     document.getElementById("noteDiv").style.display = "block";
     document.getElementById("loginDiv").style.display = "none";
 }
+ function changeTab(evt, tabId) {
+    const contents = document.getElementsByClassName("tab-content");
+    const links = document.getElementsByClassName("tablink");
+
+    // Hide all tab contents
+    for (let i = 0; i < contents.length; i++) {
+      contents[i].classList.remove("visible");
+    }
+
+    // Remove active class from all buttons
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.remove("active-tab");
+    }
+
+    // Show selected tab content
+    document.getElementById(tabId).classList.add("visible");
+
+    // Add active class to clicked tab
+    evt.currentTarget.classList.add("active-tab");
+  }
